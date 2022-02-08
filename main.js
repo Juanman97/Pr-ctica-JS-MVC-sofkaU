44228b8e6dd42c2cf78aa03bc746909a281b362a
Juan Manuel Reina <juanmreina97@gmail.com>
2022-02-08 17:33:19 -0500

    se agrega eñ trabajo del video2: clase para crear las barras, función para dibujar los elementos del tablero y plantilla de funciones para mover las barras

--- main.js
+++ main.js
@@ -14,8 +14,33 @@
     self.Board.prototype = {
         get elements() {
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
+        }
+    }
+})();
+
+//se crea el elemento bar con sus propiedades.
+(function () {
+    self.Bar = function (x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+
+        this.board.bars.push(this); //se agrega a board este elemento
+        this.kind = "rectangle"; //forma de lo que se va a dibujar
+    }
+
+    /*se modifica el modelo para agregar futuras funcionalidades para mover
+    arriba y abajo las barras */
+    self.Bar.prototype = {
+        down: function () {
+            
+        },
+        up: function () {
+
         }
     }
 })();
@@ -32,6 +57,30 @@ y se le dan como atributo al canvas */
         this.board = board;
         this.context = canvas.getContext("2d");
     }
+
+    /* función para llamar la función draw con cada elemento que tenga
+    el tablero */
+    self.BoardView.prototype = {
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--){
+                var el = this.board.elements[i];
+                draw(this.context, el);
+            }
+        }
+    }
+
+    //función para dibujar un elemento del tablero según su forma (kind).
+    //.fillRect es una función del contexto de canvas para dibujar un cuadrado
+    function draw(context, element) {
+        if (element !== null && element.hasOwnProperty("kind")) {
+           switch (element.kind) {
+            case "rectangle":
+                context.fillRect(element.x, element.y, element.width, element.height);
+                break;
+        } 
+        }
+        
+    }
 })();
 
 //en el evento "load" (carga de la pág) se llama la función main.
@@ -40,6 +89,8 @@ window.addEventListener("load", main);
 //función que crea los elementos (controlador)
 function main() {
     var board = new Board(1000, 400);
+    var bar = new Bar(20, 100, 40, 100, board);
     var canvas = document.getElementById("canvas");
     var boardView = new BoardView(canvas, board);
+    boardView.draw();
 }
